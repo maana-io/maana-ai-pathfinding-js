16a0923e56c8d3bfd6147cb138757110c0f2c8fa
Darrell Lewis-Sandy <dlewissandy@gmail.com>
2021-03-14 15:22:52 -0700

    removing more auth

--- service/src/start.dev.js
+++ service/src/start.dev.js
@@ -1,9 +1,4 @@
 import initServer from './server'
-import chalk from 'chalk'
-
-console.log(
-  chalk.bgRed('Authentication is disabled. This should not run in production')
-)
 
 initServer({
   httpAuthMiddleware: false,
